5890b51eb2040427a8468ca0213cd6390a63bce2
stilts15 <bart.friese@hotmail.com>
2024-11-03 20:54:27 +0100

    Wire templates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,7 +117,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
